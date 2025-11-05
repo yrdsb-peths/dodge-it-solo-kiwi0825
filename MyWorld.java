efe8011970d6cf1a47998761b871c0dde6b2da74
kiwi0825 <nbeinniwllek@gmail.com>
2025-11-05 14:03:10 -0500

    Added apple

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,13 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        // This creates the hero
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
+        
+        // This creates the car
+        Car car = new Car();
+        addObject (car, 600, 100);
     }
 }
